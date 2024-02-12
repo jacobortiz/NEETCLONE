ff417699d99cd9d9d0a92f1174b6d08b6532351b
Jacob Ortiz <jacobortiz90@gmail.com>
2024-02-11 22:47:13 -0800

    implemented dislike functionality

--- app/components/workspace/problem_description.tsx
+++ app/components/workspace/problem_description.tsx
@@ -19,11 +19,18 @@ export function ProblemDescription({ problem }: { problem: Problem }) {
   const [user] = useAuthState(auth)
   const { currentProblem, loading, problemDifficultyClass, setCurrentProblem } =
     useGetCurrentProblem(problem?.id)
-
   const { liked, disliked, solved, setData, starred } = useGetUserDataOnProblem(
     problem?.id
   )
   const [updating, setUpdating] = useState(false)
+
+  const getUserAndProblemData = async (transaction: any) => {
+    const userRef = doc(firestore, 'users', user!.uid)
+    const problemRef = doc(firestore, 'problems', problem.id)
+    const userDoc = await transaction.get(userRef)
+    const problemDoc = await transaction.get(problemRef)
+    return { userRef, problemRef, userDoc, problemDoc }
+  }
 
   const handleLike = async () => {
     if (!user) {
@@ -37,10 +44,8 @@ export function ProblemDescription({ problem }: { problem: Problem }) {
     setUpdating(true)
     // handle like, dislike or neither
     await runTransaction(firestore, async (transaction) => {
-      const userRef = doc(firestore, 'users', user?.uid)
-      const problemRef = doc(firestore, 'problems', problem.id)
-      const userDoc = await transaction.get(userRef)
-      const problemDoc = await transaction.get(problemRef)
+      const { userRef, problemRef, userDoc, problemDoc } =
+        await getUserAndProblemData(transaction)
 
       if (userDoc.exists() && problemDoc.exists()) {
         if (liked) {
@@ -100,6 +105,72 @@ export function ProblemDescription({ problem }: { problem: Problem }) {
     setUpdating(false)
   }
 
+  const handleDislike = async () => {
+    if (!user) {
+      toast.error('You must be logged in to like a problem', {
+        position: 'top-left',
+        theme: 'dark',
+      })
+      return
+    }
+
+    if (updating) return
+    setUpdating(true)
+
+    await runTransaction(firestore, async (transaction) => {
+      const { userRef, problemRef, userDoc, problemDoc } =
+        await getUserAndProblemData(transaction)
+
+      if (userDoc.exists() && problemDoc.exists()) {
+        //already disliked, already liked, not disliked or liked
+        if (disliked) {
+          transaction.update(userRef, {
+            dislikedProblems: userDoc
+              .data()
+              .dislikedProblems.filter((id: string) => id !== problem.id),
+          })
+
+          setCurrentProblem((prev) =>
+            prev ? { ...prev, dislikes: prev.dislikes - 1 } : null
+          )
+          setData((prev) => ({ ...prev, disliked: false }))
+        } else if (liked) {
+          transaction.update(userRef, {
+            dislikedProblems: [...userDoc.data().dislikedProblems, problem.id],
+            likedProblems: userDoc
+              .data()
+              .likedProblems.filter((id: string) => id !== problem.id),
+          })
+          transaction.update(problemRef, {
+            dislikes: problemDoc.data().dislikes + 1,
+            likes: problemDoc.data().likes - 1,
+          })
+
+          setCurrentProblem((prev) =>
+            prev
+              ? { ...prev, dislikes: prev.dislikes + 1, likes: prev.likes - 1 }
+              : null
+          )
+          setData((prev) => ({ ...prev, disliked: true, liked: false }))
+        } else {
+          transaction.update(userRef, {
+            dislikedProblems: [...userDoc.data().dislikedProblems, problem.id],
+          })
+
+          transaction.update(problemRef, {
+            dislikes: problemDoc.data().dislikes + 1,
+          })
+
+          setCurrentProblem((prev) =>
+            prev ? { ...prev, dislikes: prev.dislikes + 1 } : null
+          )
+          setData((prev) => ({ ...prev, disliked: true }))
+        }
+      }
+    })
+    setUpdating(false)
+  }
+
   return (
     <div className='bg-dark-layer-1'>
       {/* TAB */}
@@ -146,8 +217,15 @@ export function ProblemDescription({ problem }: { problem: Problem }) {
                 </div>
                 <div
                   className='flex items-center cursor-pointer hover:bg-dark-fill-3 space-x-1 rounded p-[3px]  ml-4 text-lg transition-colors 
-                  duration-200 text-green-s text-dark-gray-6'>
-                  <AiFillDislike />
+                  duration-200 text-green-s text-dark-gray-6'
+                  onClick={handleDislike}>
+                  {disliked && !updating && (
+                    <AiFillDislike className='text-dark-blue-s' />
+                  )}
+                  {!disliked && !updating && <AiFillDislike />}
+                  {updating && (
+                    <AiOutlineLoading3Quarters className='animate-spin' />
+                  )}
                   <span className='text-xs'>{currentProblem.dislikes}</span>
                 </div>
                 <div className='cursor-pointer hover:bg-dark-fill-3  rounded p-[3px]  ml-4 text-xl transition-colors duration-200 text-green-s text-dark-gray-6 '>
