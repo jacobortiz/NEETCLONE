8a7abbb1fea54c1a59b9f7ca9555637995714877
Jacob Ortiz <jacobortiz90@gmail.com>
2024-02-13 14:43:52 -0800

    fixed getData function to work in build

--- app/problems/[slug]/page.tsx
+++ app/problems/[slug]/page.tsx
@@ -3,29 +3,7 @@ import { Workspace } from '@/app/components/workspace/workspace'
 import { Problem } from '@/app/utils/problem'
 import { problems } from '@/app/utils/problems'
 
-export default async function Problems({
-  params,
-}: {
-  params: { slug: string } // TODO: Change 'string' to 'Problem',
-}) {
-  const data = await getData({ params: { slug: params.slug } })
-  return (
-    <div>
-      <Topbar problemPage={true} />
-      <Workspace problem={data.props?.problem} />
-    </div>
-  )
-}
-
-export async function generateStaticParams() {
-  const paths = Object.keys(problems).map((key) => ({
-    params: { slug: key },
-  }))
-
-  return [paths]
-}
-
-export async function getData({ params }: { params: { slug: string } }) {
+async function getData({ params }: { params: { slug: string } }) {
   const { slug } = params
   const problem = problems[slug]
 
@@ -41,4 +19,26 @@ export async function getData({ params }: { params: { slug: string } }) {
       problem,
     },
   }
+}
+
+export default async function Problems({
+  params,
+}: {
+  params: { slug: string } // TODO: Change 'string' to 'Problem',
+}) {
+  const data = await getData({ params: { slug: params.slug } })
+
+  return (
+    <div>
+      <Topbar problemPage={true} />
+      <Workspace problem={data.props?.problem} />
+    </div>
+  )
+}
+
+export async function generateStaticParams() {
+  const paths = Object.keys(problems).map((key) => ({
+    params: { slug: key },
+  }))
+  return [paths]
 }
